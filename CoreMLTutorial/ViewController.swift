7eb5d20f37bf3e1484e4bc0da856577fccbf1a5d
Cade Rock <Cade@matts-macbook-pro.local>
2018-08-08 11:57:40 -0500

    Initial Commit

---
+++ CoreMLTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreMLTutorial
+//
+//  Created by Cade Rock on 8/8/18.
+//  Copyright © 2018 Cade Rock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
